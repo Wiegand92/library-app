02b7921906287eb9083e484f8a507f2bd05c8579
wiegand92 <wiegand.alex@gmail.com>
2021-04-26 22:56:39 -0400

    update purge settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: {
     enabled: true,
     content: [
-      './src/styles/style.scss'
+      './src/styles/base/_base.scss',
+      './src/styles/components/_app.scss',
+      './src/styles/components/_book-card.scss',
+      './src/styles/components/_book-form.scss',
+      './src/styles/components/_header.scss',
+      './src/styles/components/_login-form.scss'
     ]
   },
   darkMode: false, // or 'media' or 'class'
